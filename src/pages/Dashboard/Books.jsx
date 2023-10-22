b38b90003ea95b98526bbffa3c2cc1429991c0d4
BerdiqulovN-dev <navruzbekturalov0333@gmail.com>
2023-10-22 10:22:01 +0500

    Add Books modal ended

--- src/pages/Dashboard/Books.jsx
+++ src/pages/Dashboard/Books.jsx
@@ -5,7 +5,7 @@ function Home() {
     <>
       <div className="relative overflow-x-auto shadow-md">
         <div className="userList flex justify-between items-center p-2 px-2 font-bold">
-          <h2 className="text-[26px]">Books</h2>
+          <h2 className="text-[26px]" title="Yangi kitob qo'shishdan oldin muallif nomini va kitob janrini kiritishingiz kerak">Books</h2>
           <AddBook />
         </div>
         <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
